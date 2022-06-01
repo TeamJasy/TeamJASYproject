4e243ae6c6c70997ecd6db2f7d3cad62dbb550ab
Yajahira <yari@yvelazquez.com>
2022-06-01 19:40:11 -0400

    refactored js code, added render file

--- js/app.js
+++ js/app.js
@@ -1,37 +1,30 @@
-'use strict'
-//start with storing user input and see what it looks like
-//decide on constructor format/function
+'use strict';
 
 //GLOBAL VARIABLES
 let destinationAns = [];
 let destinations = [];
+let allUserMatches = [];
 let userDestination = [];
-// let mountainVotes = 0;
-// let snowyVotes = 0;
-// let sandyVotes = 0;
+let allPhotos = ['img/aspenlake.jpeg', 'img/aspenski.jpeg', 'img/aspentown.jpeg', 'img/blow-hole.jpg', 'img/cruise.jpg', 'img/flag.jpg', 'img/hiker.jpg', 'img/mountain.jpg', 'img/trail.jpg'];
+
 
 //———————————DOM REF
 
-// Things to include in constructor:
-// Destination
 //--------------------CONSTRUCTORS
 
-function DestinationPackage(name, activity, goal, fileExtension = 'jpeg', ansKey ){
+function DestinationPackage(name, activity, goal, ansKey, photos){
   this.name = name;
   this.activity = activity;
   this.goal = goal;
-  this.photo = `img/${name}.${fileExtension}`;
+  this.photos = photos;
   this.ansKey = ansKey;
-  this.snow = 0;
-  this.mountain = 0;
-  this.sandy = 0;
+  this.userMatch = 0;
   destinations.push(this);
+  this.description = `${name} is a great location! They are known for ${activity}. You will be able to enjoy ${goal} during your stay.`;
 }
-new DestinationPackage('aspen', 'skiing', 'family activities', 'aspenlake', [0, 3, 6] )
-new DestinationPackage('ensenada', 'swimming', 'family relaxation','meow', [2, 5, 8] )
-new DestinationPackage('mountain', 'hiking', 'isolation', [1, 4, 7])
-// let formData = new Destination();
-
+new DestinationPackage('Aspen', 'skiing', 'family activities', [0, 3, 6], [allPhotos[0], allPhotos [1], allPhotos[2]]);
+new DestinationPackage('Ensenada', 'swimming', 'family relaxation', [2, 5, 8], [allPhotos[3], allPhotos [4], allPhotos[5]]);
+new DestinationPackage('Appalachian Mountains', 'hiking', 'isolation', [1, 4, 7], [allPhotos[6], allPhotos [7], allPhotos[8]]);
 
 //--------------------HELPER FUNCTIONS
 
@@ -41,65 +34,45 @@ new DestinationPackage('mountain', 'hiking', 'isolation', [1, 4, 7])
 
 
 //--------------------FUNCTIONS
-function renderDestination(){
-  submitResults();
-  let renderData = localStorage.getItem ('storedDestination');
-  let parsedData = JSON.parse(renderData);
-
-  console.log()
-
-  // document.getElementById('currentDest');
-  // if (destinationAns [0] === destinations [0]) {
-    
-  // }
-   
+function selectUserMatch(){
+  for( let i = 0; i < destinations.length; i++){
+    allUserMatches.push(destinations[i].userMatch);
+  }
+  let userDestinationIndex = Math.max(...allUserMatches);
+  console.log(userDestinationIndex);
+  for(let j = 0; j < destinations.length; j++){
+    if(userDestinationIndex === destinations[j].userMatch){
+      userDestination = destinations[j];
+      let stringDestination = JSON.stringify(userDestination);
+      localStorage.setItem('storedDestination', stringDestination);
+    }
+  }
 }
 
-
 //--------------------EVENT LISTENERS
-let quizForm = document.getElementById('quiz')
-quizForm.addEventListener('submit', submitResults)
+let quizForm = document.getElementById('quiz');
+quizForm.addEventListener('submit', submitResults);
 
 
 
 //--------------------EVENT HANDLERS
-// function submitResults(event){
-//   event.preventDefault()
-// for(let i = 0; i < 9; i++){
-//   if(quizForm[i].checked){destinationAns.push(i)}
-// }
-// for(let j = 0; j < destinationAns.length; j++){
-//   let userMatch = 0
-//   for(let k = 0; k < destinations.length; k++){
-//   if(destinationAns[k] === destinations[j].ansKey[k]){
-//     userMatch++
-//   }
-// }
-// console.log(userMatch)
-// if (userMatch === destinationAns.length){
-//   userDestination.push(destinations[j])
-// }
-// let stringDestination = JSON.stringify(userDestination)
-// localStorage.setItem('storedDestination', stringDestination)
-// }
-// }
-// function submitResults(event){
-//   event.preventDefault(); 
-//   if(document.getElementById('q1-1').checked){
-//     destinations.snow++
-//   }
-//   if(document.getElementById('q1-2').checked){
-//     destinations.mountain++
-//   }
-//   if(document.getElementById('q1-3').checked){
-//     destinations.sandy++
-//   }
+function submitResults(event){
+  event.preventDefault();
+  for(let i = 0; i < 10; i++){
+    if(quizForm[i].checked){destinationAns.push(i);}
+  }
+  for(let j = 0; j < destinationAns.length; j++){
+    for(let k = 0; k < destinations.length; k++){
+      if(destinationAns[k] === destinations[j].ansKey[k]){
+        destinations[j].userMatch++;
+      }
+    }
+  }
+  window.location.href = "uservacations.html" ;
+  selectUserMatch();
+}
 
-// }
-// function recordAns(){
-//   let formAns = document.getElementById('currentDest');
-//   formAns.innerHTML = formData;
-// }
 
 
 //--------------------FUNCTION CALLS
+
